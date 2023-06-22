868ac85f08e5b1e7e8c970f1a20ebb945a8f5d25
tatarynrm <tatarynrm@gmail.com>
2023-06-22 11:51:55 +0300

    code sex

--- index.js
+++ index.js
@@ -110,14 +110,14 @@ app.post("/mail-send", async (req, res) => {
 
 // WEB SOCKETS------------------------------------------------------------------------
 const io = new Server(server, {
-  // cors: {
-  //   origin: "http://192.168.5.180",
-  //   methods: ["GET", "POST"],
-  // },
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://192.168.5.180",
     methods: ["GET", "POST"],
   },
+  // cors: {
+  //   origin: "http://localhost:3000",
+  //   methods: ["GET", "POST"],
+  // },
 });
 
 let onlineUsers = [];
